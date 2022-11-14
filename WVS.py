7a91b6af4b3d07ba3f52497a78a7648542db2a42
wakanameko <63937252+wakanameko@users.noreply.github.com>
2022-11-14 23:10:58 +0900

    Add files via upload

--- WVS.py
+++ WVS.py
@@ -1,16 +1,9 @@
 # coding: UTF-8
 import tkinter as tk
 import platform
-import os
 from tkinter import messagebox
-if os.name == 'posix':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。エラーが発生しても自己責任でお願いします。')
-if os.name == 'linux':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。エラーが発生しても自己責任でお願いします。')
-if os.name == 'darwin':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。エラーが発生しても自己責任でお願いします。')
-
-print('WindowsVerShower_V1.0')
-print('Developer @wakanameko2')
-
+print('WindowsVerShower_V1.3\nDeveloper @wakanameko2')
 baseGround = tk.Tk()
 baseGround.withdraw()
-
-if os.name == 'nt':messagebox.showinfo('WindowsVerShower', platform.platform())
+if not platform.system() == 'Windows':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。エラーが発生しても自己責任でお願いします。')
+if platform.system() == 'Windows':messagebox.showinfo('WindowsVerShower', platform.platform())
