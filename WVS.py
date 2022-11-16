eb7b61e8bcda8fd293912e20a899c65461c71796
wakanameko <63937252+wakanameko@users.noreply.github.com>
2022-11-16 12:47:43 +0900

    Update WVS.py

--- WVS.py
+++ WVS.py
@@ -5,5 +5,5 @@ from tkinter import messagebox
 print('WindowsVerShower_V1.3\nDeveloper @wakanameko2')
 baseGround = tk.Tk()
 baseGround.withdraw()
-if not platform.system() == 'Windows':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。エラーが発生しても自己責任でお願いします。')
+if not platform.system() == 'Windows':messagebox.showerror('Attention','Windows以外のOSでの実行は想定されていません。\nエラーが発生しても自己責任でお願いします。')
 if platform.system() == 'Windows':messagebox.showinfo('WindowsVerShower', platform.platform())
